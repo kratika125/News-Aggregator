c2d7537ec0127948bfb64c288c4e848d6b970e5d
kratika125 <kratikabhadauria21@gmail.com>
2024-07-11 03:31:03 +0530

    news aggregator

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import AllNews from "./Components/AllNews";
+ import Footer from "./Components/Footer";
+import TopHeadlines from "./Components/TopHeadlines";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CountryNews from "./Components/CountryNews";
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="w-full">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AllNews />} />
+          <Route path="/top-headlines/:category" element={<TopHeadlines />} />
+          <Route path="/country/:iso" element={<CountryNews />} />
+        </Routes>
+        {/* <Cards />  */}
+        {/* <Footer />   */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
